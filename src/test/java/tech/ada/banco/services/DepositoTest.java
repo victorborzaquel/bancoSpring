00c0ec6a4aa914040c5dd8144aa46db76a8de8f8
Victor Borzaquel <victorborzaquel@outlook.com>
2023-02-12 13:38:49 -0300

    Deposito Test

--- src/test/java/tech/ada/banco/services/DepositoTest.java
+++ src/test/java/tech/ada/banco/services/DepositoTest.java
@@ -1,77 +1,121 @@
 package tech.ada.banco.services;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import tech.ada.banco.controller.BaseContaControllerTest;
 import tech.ada.banco.exceptions.ResourceNotFoundException;
+import tech.ada.banco.exceptions.ValorInvalidoException;
 import tech.ada.banco.model.Conta;
-import tech.ada.banco.model.ModalidadeConta;
-import tech.ada.banco.repository.ContaRepository;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-class DepositoTest extends BaseContaControllerTest {
-    private final ContaRepository repository = Mockito.mock(ContaRepository.class);
+class DepositoTest extends BaseContaTest {
     private final Deposito deposito = new Deposito(repository);
 
     @Test
-    void testSaqueProblemaDeBancoDeDados() {
-        Conta conta = new Conta(ModalidadeConta.CC, null);
-        conta.deposito(BigDecimal.TEN);
-        when(repository.findContaByNumeroConta(10)).thenThrow(RuntimeException.class);
-        assertEquals(BigDecimal.valueOf(10), conta.getSaldo(), "O saldo inicial da conta deve ser alterado para 10");
+    void testProblemaDeBancoDeDados() {
+        final BigDecimal VALOR_INICIAL = BigDecimal.valueOf(7);
+        final BigDecimal VALOR_DEPOSITO = BigDecimal.valueOf(1);
+        final BigDecimal VALOR_FINAL = BigDecimal.valueOf(7);
 
-        try {
-            // TODO
-            deposito.executar(1, BigDecimal.ONE);
-            fail("A conta deveria não ter sido encontrada. Por problema de conexão de banco de dados");
-        } catch (RuntimeException e) {
+        Conta conta = criarConta(VALOR_INICIAL, numeroContaOrigem);
 
-        }
+        when(repository.save(any())).thenThrow(RuntimeException.class);
+
+        assertThrows(RuntimeException.class, () -> deposito.executar(numeroContaDestino, VALOR_DEPOSITO), "O saque não pode ser realizado. Por problema de conexão de banco de dados");
 
         verify(repository, times(0)).save(any());
-        assertEquals(BigDecimal.valueOf(10), conta.getSaldo(), "O saldo da conta não pode ter sido alterado.");
+        assertEquals(VALOR_FINAL, conta.getSaldo(), "O saldo da conta não pode ter sido alterado.");
     }
 
     @Test
-    void testDepositoContaNaoEncontrada() {
-        Conta conta = criarConta(BigDecimal.valueOf(7));
+    void testContaNaoEncontrada() {
+        final BigDecimal VALOR_INICIAL = BigDecimal.valueOf(7);
+        final BigDecimal VALOR_DEPOSITO = BigDecimal.valueOf(1);
+        final BigDecimal VALOR_FINAL = BigDecimal.valueOf(7);
 
-        assertThrows(ResourceNotFoundException.class, () -> deposito.executar(numeroContaInexistente, BigDecimal.ONE), "A conta não foi encontrada.");
+        Conta conta = criarConta(VALOR_INICIAL, numeroContaOrigem);
+
+        assertThrows(ResourceNotFoundException.class, () -> deposito.executar(numeroContaInexistente, VALOR_DEPOSITO), "O deposito não pode ser realizado. Por problema de conexão de banco de dados");
+
         verify(repository, times(0)).save(any());
-        assertEquals(BigDecimal.valueOf(7), conta.getSaldo());
-    }
-
-    @Test
-    void testProblemaBancoDeDados() {
-
+        assertEquals(VALOR_FINAL, conta.getSaldo(), "O saldo da conta não pode ter sido alterado.");
     }
 
     @Test
     void testDepositoNegativo() {
+        final BigDecimal VALOR_INICIAL = BigDecimal.valueOf(7);
+        final BigDecimal VALOR_DEPOSITO = BigDecimal.valueOf(-1);
+        final BigDecimal VALOR_FINAL = BigDecimal.valueOf(7);
 
+        Conta destino = criarConta(VALOR_INICIAL, numeroContaDestino);
+
+        assertThrows(ValorInvalidoException.class, () -> deposito.executar(numeroContaDestino, VALOR_DEPOSITO), "O deposito não pode ser realizado. Valor inválido.");
+
+        verify(repository, times(0)).save(any());
+        assertEquals(VALOR_FINAL, destino.getSaldo(), "O saldo da conta não pode ter sido alterado.");
     }
 
     @Test
-    void testDepositoNormal() {
+    void testDepositoValorInteiro() {
+        final BigDecimal VALOR_INICIAL = BigDecimal.valueOf(7);
+        final BigDecimal VALOR_DEPOSITO = BigDecimal.valueOf(1);
+        final BigDecimal VALOR_FINAL = BigDecimal.valueOf(8).setScale(2, RoundingMode.HALF_UP);
 
+        Conta destino = criarConta(VALOR_INICIAL, numeroContaDestino);
+
+        BigDecimal saldo = deposito.executar(numeroContaDestino, VALOR_DEPOSITO);
+
+        verify(repository, times(1)).save(any());
+        assertEquals(VALOR_FINAL, saldo);
+        assertEquals(VALOR_FINAL, destino.getSaldo());
     }
 
     @Test
     void testDepositoCentavos() {
+        final BigDecimal VALOR_INICIAL = BigDecimal.valueOf(7.85);
+        final BigDecimal VALOR_DEPOSITO = BigDecimal.valueOf(0.15);
+        final BigDecimal VALOR_FINAL = BigDecimal.valueOf(8).setScale(2, RoundingMode.HALF_UP);
 
+        Conta destino = criarConta(VALOR_INICIAL, numeroContaDestino);
+
+        BigDecimal saldo = deposito.executar(numeroContaDestino, VALOR_DEPOSITO);
+
+        verify(repository, times(1)).save(any());
+        assertEquals(VALOR_FINAL, saldo);
+        assertEquals(VALOR_FINAL, destino.getSaldo());
     }
 
     @Test
     void testArredondamentoParaCima() {
+        final BigDecimal VALOR_INICIAL = BigDecimal.valueOf(7);
+        final BigDecimal VALOR_DEPOSITO = BigDecimal.valueOf(0.255);
+        final BigDecimal VALOR_FINAL = BigDecimal.valueOf(7.26);
 
+        Conta destino = criarConta(VALOR_INICIAL, numeroContaDestino);
+
+        BigDecimal saldo = deposito.executar(numeroContaDestino, VALOR_DEPOSITO);
+
+        verify(repository, times(1)).save(any());
+        assertEquals(VALOR_FINAL, saldo);
+        assertEquals(VALOR_FINAL, destino.getSaldo());
     }
 
     @Test
     void testArredondamentoParaBaixo() {
+        final BigDecimal VALOR_INICIAL = BigDecimal.valueOf(7);
+        final BigDecimal VALOR_DEPOSITO = BigDecimal.valueOf(0.254);
+        final BigDecimal VALOR_FINAL = BigDecimal.valueOf(7.25);
 
+        Conta destino = criarConta(VALOR_INICIAL, numeroContaDestino);
+
+        BigDecimal saldo = deposito.executar(numeroContaDestino, VALOR_DEPOSITO);
+
+        verify(repository, times(1)).save(any());
+        assertEquals(VALOR_FINAL, saldo);
+        assertEquals(VALOR_FINAL, destino.getSaldo());
     }
 }
