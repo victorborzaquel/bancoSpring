3141399cdf1226ea744264d45c7927aa9111cbe7
Victor Borzaquel <victorborzaquel@outlook.com>
2023-02-12 13:35:35 -0300

    Base Conta

--- src/test/java/tech/ada/banco/services/BaseContaTest.java
+++ src/test/java/tech/ada/banco/services/BaseContaTest.java
@@ -1,9 +1,8 @@
 package tech.ada.banco.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 import tech.ada.banco.model.Conta;
 import tech.ada.banco.model.ModalidadeConta;
 import tech.ada.banco.repository.ContaRepository;
@@ -18,26 +17,16 @@ import static org.mockito.Mockito.when;
 @AutoConfigureMockMvc
 public abstract class BaseContaTest {
 
-    @Autowired
-    protected MockMvc mvc;
-
-    @Autowired
-    protected ContaRepository repository;
-
+    protected ContaRepository repository = Mockito.mock(ContaRepository.class);
     protected int numeroContaInexistente = 9999;
+    protected int numeroContaOrigem = 1;
+    protected int numeroContaDestino = 2;
 
-    protected Conta criarConta(double valor, int numeroConta) {
-        Conta conta = new Conta(ModalidadeConta.CC, null);
-        conta.deposito(BigDecimal.valueOf(valor));
+    protected Conta criarConta(BigDecimal valor, int numeroConta) {
+        Conta conta = new Conta(ModalidadeConta.CP, null);
+        conta.deposito(valor);
         when(repository.findContaByNumeroConta(numeroConta)).thenReturn(Optional.of(conta));
-        assertEquals(BigDecimal.valueOf(valor), conta.getSaldo(),
-                "O saldo inicial da conta deve ser alterado para " + valor);
+        assertEquals(valor, conta.getSaldo(), "O saldo inicial da conta deve ser alterado para " + valor);
         return conta;
     }
-
-    protected Conta obtemContaDoBanco(Conta contaBase) {
-        return repository.findContaByNumeroConta(contaBase.getNumeroConta())
-                .orElseThrow(NullPointerException::new);
-    }
-
 }
