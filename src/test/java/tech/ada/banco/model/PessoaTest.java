bdbcac8a488a942047a301fdfb4a24d85e249dee
Victor Borzaquel <victorborzaquel@outlook.com>
2023-02-12 13:38:54 -0300

    Pessoa Test

--- src/test/java/tech/ada/banco/model/PessoaTest.java
+++ src/test/java/tech/ada/banco/model/PessoaTest.java
@@ -1,15 +1,47 @@
 package tech.ada.banco.model;
 
 import org.junit.jupiter.api.Test;
+import tech.ada.banco.exceptions.ValorInvalidoException;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PessoaTest {
 
     @Test
     void testMenorDeIdade() {
+        LocalDate dataNascimento = LocalDate.now().minusYears(17);
+
+        assertThrows(ValorInvalidoException.class, () -> new Pessoa("João", "123.456.789-00", dataNascimento));
+    }
+
+    @Test
+    void testSetMenorDeIdade() {
+        LocalDate dataNascimento = LocalDate.now().minusYears(18);
+        LocalDate dataNascimentoFinal = LocalDate.now().minusYears(17);
+        Pessoa pessoa = new Pessoa("João", "123.456.789-00", dataNascimento);
+
+        assertThrows(ValorInvalidoException.class, () -> pessoa.setDataNascimento(dataNascimentoFinal));
     }
 
     @Test
     void testMaiorDeIdade() {
+        LocalDate dataNascimento = LocalDate.now().minusYears(18);
+        Pessoa pessoa = new Pessoa("João", "123.456.789-00", dataNascimento);
 
+        assertEquals(dataNascimento, pessoa.getDataNascimento());
+    }
+
+    @Test
+    void testSetMaiorDeIdade() {
+        LocalDate dataNascimento = LocalDate.now().minusYears(18);
+        LocalDate dataNascimentoFinal = LocalDate.now().minusYears(19);
+        Pessoa pessoa = new Pessoa("João", "123.456.789-00", dataNascimento);
+
+        pessoa.setDataNascimento(dataNascimentoFinal);
+
+        assertEquals(dataNascimentoFinal, pessoa.getDataNascimento());
     }
 }
